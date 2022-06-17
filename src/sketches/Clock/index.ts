839b74685e88898c50c65adfbff5d49b04ac61c4
Daniel Still <80142739+StillScripts@users.noreply.github.com>
2022-06-17 21:17:11 +1000

    Add circle
    Put a circle in the middle of the clock to make it look more realistic.

--- src/sketches/Clock/index.ts
+++ src/sketches/Clock/index.ts
@@ -3,7 +3,6 @@ import { Clock } from "./classes";
 
 /**
  * This sketch is creates a clock with an hour, minute and seconds hand.
- *
  * @param {p5} p - The p5.js object
  *
  */
@@ -11,13 +10,16 @@ export default function clockSketch(p: p5) {
   let clock: Clock;
 
   p.setup = () => {
-		p.createCanvas(400, 400);
-		p.stroke(255);
-		clock = new Clock(p.min(p.width, p.height) / 2);
+    p.createCanvas(400, 400);
+    p.stroke(255);
+    clock = new Clock(p.min(p.width, p.height) / 2);
   };
 
   p.draw = () => {
-		p.background(10);
-		clock.tick(p, p.width / 2, p.height / 2);
+    p.background(10);
+    clock.tick(p, p.width / 2, p.height / 2);
+    p.push();
+    p.circle(p.width / 2, p.height / 2, 8);
+    p.pop();
   };
 }
