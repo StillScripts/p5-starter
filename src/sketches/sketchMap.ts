15c7633de3326d65d39d371b2468b2646ec924f9
Daniel Still <80142739+StillScripts@users.noreply.github.com>
2022-06-18 10:35:45 +1000

    Add Bouncy Bubbles animation
    Finish the bouncy bubbles animation and add it to the sketchMap

--- src/sketches/sketchMap.ts
+++ src/sketches/sketchMap.ts
@@ -1,13 +1,14 @@
 import type p5 from "p5";
-
+import bouncyBubbleSketch from "./BouncyBubbles";
 import clockSketch from "./Clock";
 import particleSystemSketch from "./ParticleSystem";
-import rotatingShapesSketch from "./RotatingShapes";
+import shapesSketch from "./Shapes";
 
 export const sketchMap: Record<string, (p: p5) => void> = {
-  RotatingShapes: rotatingShapesSketch,
+  Shapes: shapesSketch,
   Clock: clockSketch,
   ParticleSystem: particleSystemSketch,
+  BouncyBubbles: bouncyBubbleSketch,
 } as const;
 
 export type SketchKey = keyof typeof sketchMap;
