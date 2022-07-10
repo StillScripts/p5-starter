57c1e9c5e729be93b4e4ad8c98825cb8b7686b9d
Daniel Still <80142739+StillScripts@users.noreply.github.com>
2022-07-10 22:25:18 +1000

    Apply the select and container functions
    Use the functions from the SketchSelector and SketchContainer components.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import "./style.css";
-import p5 from "p5";
 import { SketchKey, sketchMap } from "./sketches/sketchMap";
 import { Header, SketchSelector, SketchContainer } from "./components";
-import { convertToParams } from "./utils/stringFunctions";
-import { getSketchFromParams, redirectUrl } from "./utils/urlParams";
+import { getSketchFromParams } from "./utils/urlParams";
+import { addListenerToSelect } from "./components/SketchSelector/SketchSelector";
+import { renderSketch } from "./components/SketchContainer/SketchContainer";
 
 /**
  * Initialise the application by getting the sketch from the url parameter,
@@ -13,9 +13,11 @@ import { getSketchFromParams, redirectUrl } from "./utils/urlParams";
 function init() {
   const CONTAINER_ID = "sketch-container";
   const SELECT_ID = "animation-selector";
-  const sketch: SketchKey = getSketchFromParams(Object.keys(sketchMap)[0]); // get the sketch to render
 
+  const sketch: SketchKey = getSketchFromParams(Object.keys(sketchMap)[0]); // get the sketch to render
   const app = document.querySelector<HTMLDivElement>("#app")!; // get the app container <div> element
+
+  // Update the innerHTML of the app container to contain the header, select input, and the sketch.
   app.innerHTML = `
     ${Header(`${sketch}`)}
     <main>
@@ -24,36 +26,10 @@ function init() {
     </main>
   `;
 
+  
+  addListenerToSelect(SELECT_ID, sketch); 
 
-  try {
-    // Add an event listener to the select element
-    document.querySelector("#" + SELECT_ID)?.addEventListener("change", (e) => {
-      const select = e.target as HTMLSelectElement;
-      const value = select.value as SketchKey;
-      if (value !== sketch) {
-        // Change the sketch if a new sketch option has been selected
-        redirectUrl(window.location.href.split("?")[0], {
-          sketch: convertToParams(value),
-        });
-      }
-    });
-
-    // Get the sketch container
-    const container = document.querySelector<HTMLDivElement>("#" + CONTAINER_ID);
-    // Remove existing animation
-    container?.removeChild(container.childNodes[0]);
-    // Make a div for the sketch
-    const sketchDiv = document.createElement("div");
-    // Give the div an id
-    sketchDiv.id = "sketch";
-    // Add this div to the sketch container
-    container?.appendChild(sketchDiv);
-
-    // Initialise the p5.js object which contains the sketch
-    new p5(sketchMap[sketch], sketchDiv);
-  } catch (error) {
-    console.log("Error with initiating app: ", error);
-  }
+  renderSketch(CONTAINER_ID, sketch);
 }
 
 init();
